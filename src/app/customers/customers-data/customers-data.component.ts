f13082f8edb1a4f7497cc8dde42ee5a4245af482
n0urs <20nourabdulnasser00@gmail.com>
2024-07-15 02:27:16 +0300

    table adjustments

--- src/app/customers/customers-data/customers-data.component.ts
+++ src/app/customers/customers-data/customers-data.component.ts
@@ -5,40 +5,47 @@ import { TransactionService } from 'src/app/transactions.service';
 @Component({
   selector: 'app-customer-table',
   templateUrl: './customers-data.component.html',
-  styleUrls: ['./customers-data.component.css']
+  styleUrls: ['./customers-data.component.css'],
 })
 export class CustomersDataComponent implements OnInit {
   customers: any[] = [];
   transactions: any[] = [];
   filteredCustomers: any[] = [];
+
   @Output() customerSelected = new EventEmitter<number>();
 
   constructor(
     private customerService: CustomerService,
     private transactionService: TransactionService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.customerService.getCustomers().subscribe(customers => {
+    this.customerService.getCustomers().subscribe((customers) => {
       this.customers = customers;
       this.filteredCustomers = customers;
     });
 
-    this.transactionService.getTransactions().subscribe(transactions => {
+    this.transactionService.getTransactions().subscribe((transactions) => {
       this.transactions = transactions;
     });
   }
 
   filterTable(filterText: string): void {
-    this.filteredCustomers = this.customers.filter(customer =>
-      customer.name.toLowerCase().includes(filterText.toLowerCase()));
+    this.filteredCustomers = this.customers.filter((customer) =>
+      customer.name.toLowerCase().includes(filterText.toLowerCase())
+    );
   }
 
   getTransactionsForCustomer(customerId: number): any[] {
-    return this.transactions.filter(transaction => transaction.customer_id == customerId);
+    return this.transactions.filter(
+      (transaction) => transaction.customer_id == customerId
+    );
   }
 
   selectCustomer(customerId: number): void {
     this.customerSelected.emit(customerId);
   }
+
+  
+  
 }
