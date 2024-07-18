c5ccc40d4e8b8a3a5416bb673aaebbb3db6e5d07
n0urs <20nourabdulnasser00@gmail.com>
2024-07-18 22:53:50 +0300

    api hosted

--- src/app/customers.service.ts
+++ src/app/customers.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
-  private baseUrl = 'assets/db.json/';
+  private baseUrl = '../assets/db.json/';
 
   constructor(private http: HttpClient) {}
 
   getCustomers(): Observable<any> {
-    return this.http.get(this.baseUrl + 'customers');
+    return this.http.get<any>(this.baseUrl).pipe(map((data) => data.customers));
   }
 }
--- src/app/transactions.service.ts
+++ src/app/transactions.service.ts
@@ -1,29 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class TransactionService {
-
-  public customerSelected = new BehaviorSubject(0)
+  public customerSelected = new BehaviorSubject(0);
   currentCustomer = this.customerSelected.asObservable();
 
-  private baseUrl = 'assets/db.json/';
+  private baseUrl = '../assets/db.json/';
 
   constructor(private http: HttpClient) {}
 
   getTransactions(): Observable<any> {
-    return this.http.get(this.baseUrl + 'transactions');
+    return this.http
+      .get<any>(this.baseUrl)
+      .pipe(map((data) => data.transactions));
   }
 
-  setSelectedCustomer(customerSelected:any){
-
-    this.customerSelected.next(customerSelected)
+  setSelectedCustomer(customerSelected: any) {
+    this.customerSelected.next(customerSelected);
     // console.log(customerSelected, "from service");
-    
   }
-
-  }
-
+}
