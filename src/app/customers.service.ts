55647e4c756bcf7dda79e9aee9f728608c9006fb
n0urs <20nourabdulnasser00@gmail.com>
2024-07-18 23:47:02 +0300

    api hosted

--- src/app/customers.service.ts
+++ src/app/customers.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
-  private baseUrl = 'https://gist.githubusercontent.com/nour-abdulnasser/b8258ac15e7ea565388c65a4e429412c/raw/a328acce237d1fabe8a5602eba09afae69c0dd3f/db.json';
+  private baseUrl = 'assets/customers.json';
 
   constructor(private http: HttpClient) {}
 
   getCustomers(): Observable<any> {
-    return this.http.get<any>(this.baseUrl).pipe(map((data) => data.customers));
+    return this.http.get(this.baseUrl);
   }
 }
--- src/app/transactions.service.ts
+++ src/app/transactions.service.ts
@@ -9,8 +9,8 @@ export class TransactionService {
   public customerSelected = new BehaviorSubject(0);
   currentCustomer = this.customerSelected.asObservable();
 
-  private baseUrl = 'https://gist.githubusercontent.com/nour-abdulnasser/b8258ac15e7ea565388c65a4e429412c/raw/a328acce237d1fabe8a5602eba09afae69c0dd3f/db.json';
-
+  // private baseUrl = 'https://gist.githubusercontent.com/nour-abdulnasser/b8258ac15e7ea565388c65a4e429412c/raw/a328acce237d1fabe8a5602eba09afae69c0dd3f/db.json';
+private baseUrl = "assets/.transactions.json"
   constructor(private http: HttpClient) {}
 
   getTransactions(): Observable<any> {
